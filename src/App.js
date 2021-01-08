ad31c2c043c6dda774f8a7caeff17af2f5b312e8
Alexandre Blache <alexandreblache.pro@gmail.com>
2021-01-08 11:25:17 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function App() {
+  return (
+      <div>
+        <nav className="nav flex-column">
+          <a className="nav-link active" href="/">Accueil</a>
+          <a className="nav-link" href="/">A propos</a>
+          <a className="nav-link" href="/">Services</a>
+          <a className="nav-link" href="/">Contact</a>
+        </nav></div>
+
+  );
+}
+
+export default App;
